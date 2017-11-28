92081f55ed5d0a4e2ce3af02e45efa31e04e9d54
Saravanan G <sgoffl1603@gmail.com>
2017-11-28 20:42:29 +0530

    Added HTTP404 error

--- newproject/music/views.py
+++ newproject/music/views.py
@@ -1,20 +1,19 @@
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Album
-from django.template import loader
+from django.shortcuts import render
 def index(request):
 
     all_albums= Album.objects.all()
-    template =loader.get_template('music/index.html')
-    context = {
-        'all_albums':all_albums,
 
-    }
-
-    return HttpResponse(template.render(context,request))
-
+    return render(request,'music/index.html',{'all_albums':all_albums})
 
 
 
 def detail(request,album_id):
 
-    return HttpResponse("<h2>Details for Album id </h2>"+album_id)
+    try:
+      album= Album.objects.get(pk=album_id)
+
+    except Album.DoesNotExists:
+        raise Http404("Album does not exists")
+    return render(request,'music/detail.html',{'album':album})
